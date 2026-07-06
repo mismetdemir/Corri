17c0f493231712972659eaf8d2402b88453d4f0d
mismetdemir <mismetdemir@hotmail.com>
2026-07-06 11:05:16 +0300

    Change deployment settings to global

--- deploy-commands.js
+++ deploy-commands.js
@@ -56,13 +56,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 try {
   console.log("Registering commands...");
 
-  await rest.put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID,
-    ),
-    { body: commands },
-  );
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: commands,
+  });
 
   console.log("Commands registered successfully.");
 } catch (error) {
